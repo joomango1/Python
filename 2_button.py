af52131f74516d82bc473d191bc732a9b45282ab
MinSung Joo <dud47889@naver.com>
2023-08-20 20:52:44 +0900

    first commit

---
+++ 2_button.py
@@ -0,0 +1,15 @@
+from tkinter import*
+
+root = Tk()
+root.title("Nado GUI")
+
+btn1 = Button(root, text = "버튼 1")
+btn1.pack()
+
+btn2 = Button(root, padx=5, pady=10 ,text="버튼 2" )
+btn2.pack()
+
+btn3 = Button(root, padx=10, pady=5, text = "버튼 3" )
+btn3.pack() 
+
+root.mainloop()
